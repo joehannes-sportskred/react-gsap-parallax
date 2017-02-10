c7069dc6f5d39764471a260f391e53441083923d
Andrew Monks <a@monks.co>
2017-02-10 12:51:46 -0600

    complicate demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,43 +3,35 @@ import {render} from 'react-dom'
 
 import { ParallaxContainer, Parallax } from '../../src'
 
-const blockStyle = {
-  background: '#666',
-  width: '400px',
-  height: '200px',
-  backgroundSize: 'cover !important',
-  boxShadow: '0 0 25px 0 rgba(0,0,0,0.8)'
-}
-
 const Demo = () => (
   <ParallaxContainer
     scrollDistance={5000}
     scrolljack={false}
-    id='parallax'
   >
     {/***********************************************/}
     <Parallax
-      style={{top: '50vh'}}
+      style={{x: 0, y: 0}}
       keyframes={{
-        '0%': {left: '-500px'},
-        '20%': {left: '-500px'},
-        '30%': {left: '100px'},
-        '100%': {left: '100px'}
+        '0vh': {top: '10vh', left: '10vh', fontSize: '12px'},
+        '50vh': {top: '20vh', left: '50vh', fontSize: '36px'},
+        '100vh': {top: '100vh', left: '90vh', fontSize: '36px'}
       }}
     >
-      <div style={{...blockStyle, background: 'url(http://files3.porsche.com/filestore/image/multimedia/none/jdp-2016-991-2nd-c2-modelimage-key-features/normal/95bef184-905e-11e6-9f1b-0019999cd470/porsche-normal.jpg) no-repeat center center'}} />
+      <div>
+        THING!!!!!
+      </div>
     </Parallax>
     {/***********************************************/}
     <Parallax
-      style={{top: '50vh'}}
       keyframes={{
-        '0%': {left: '150%'},
-        '20%': {left: '150%'},
-        '30%': {left: '80%'},
-        '100%': {left: '80%'}
+        '0vh': {top: '10vh', left: '10vh', fontSize: '12px'},
+        '50vh': {top: '60vh', left: '80vh', fontSize: '100px'},
+        '100vh': {top: '80vh', left: '90vh', fontSize: '36px'}
       }}
     >
-      <div style={{...blockStyle, background: 'url(http://files1.porsche.com/filestore/wallpaper/multimedia/none/991-2nd-c4s-gallery-wallpaper-08/wallpaper/c0992e1e-1dcd-11e6-9225-0019999cd470;l63599685700;w1920;h1080/porsche-wallpaper.jpg) no-repeat center center'}} />
+      <div>
+        THING!!!!!
+      </div>
     </Parallax>
     {/***********************************************/}
   </ParallaxContainer>
