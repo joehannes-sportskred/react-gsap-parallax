dd2f3ea60f2d3f78f4c1fe8def69f39cbd7b3951
Andrew Monks <a@monks.co>
2017-02-10 23:53:58 -0600

    add webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,43 @@
+import R from 'ramda'
+import { resolve } from 'path'
+
+import {
+  addBabel,
+  addExtern,
+  addSourcemap,
+  addMinify,
+  addName,
+  addProduction,
+  addProgress,
+  addCJS2,
+  addUMD,
+  base
+} from '@amonks/webpack-helpers'
+
+const packageName = require('./package.json').name
+
+const addSrc = R.pipe(
+  R.assoc('entry', [resolve(__dirname, 'src/index.js')])
+)
+
+const common = R.pipe(
+  addSrc,
+  addSourcemap,
+  addBabel,
+  addProgress,
+  addExtern('react', 'React'),
+  addExtern('gsap', 'TweenMax'),
+  addName(packageName)
+)(base)
+
+const config = [
+  addUMD(common),
+  addMinify(addUMD(addProduction(common))),
+  addCJS2(common),
+  addMinify(addCJS2(addProduction(common)))
+]
+
+console.log(JSON.stringify(config, undefined, 2))
+
+export default config
+
