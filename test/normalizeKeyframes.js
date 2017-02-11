e1bd0c978ad5ded53dba4768e11f1c9e243cd8bf
Andrew Monks <a@monks.co>
2017-02-10 23:39:36 -0600

    add tests

---
+++ test/normalizeKeyframes.js
@@ -0,0 +1,29 @@
+import { test } from 'ava'
+import normalizeKeyframes from '../src/normalizeKeyframes'
+
+test('throws an error if passed an empty object', t => {
+  try {
+    normalizeKeyframes({})
+    t.fail()
+  } catch (e) {
+    t.pass()
+  }
+})
+
+test('makes front and back if passed one keyframe', t => {
+  const result = normalizeKeyframes({'50%': 'value'})
+  t.is(3, result.length)
+  t.deepEqual(result[0], {key: 0, value: 'value', duration: 50})
+  t.deepEqual(result[1], {key: 50, value: 'value', duration: 50})
+  t.deepEqual(result[2], {key: 100, value: 'value', duration: 0})
+})
+
+test('throws an error if passed an array', t => {
+  try {
+    normalizeKeyframes([])
+    t.fail()
+  } catch (e) {
+    t.pass()
+  }
+})
+
