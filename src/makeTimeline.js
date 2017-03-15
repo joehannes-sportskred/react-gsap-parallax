47e845e2b46c5fda1009f5b39eb368a9c522429c
Andrew Monks <a@monks.co>
2017-03-15 16:54:02 -0500

    feat: add warnings for slow-animating properties

--- src/makeTimeline.js
+++ src/makeTimeline.js
@@ -4,6 +4,35 @@ import R from 'ramda'
 
 import normalizeKeyframes from './normalizeKeyframes'
 
+const checkForBadProperties = (() => {
+  const badProperties = [
+    'fontSize',
+    'margin', 'marginTop', 'marginLeft', 'marginRight', 'marginBottom',
+    'padding', 'paddingTop', 'paddingLeft', 'paddingRight', 'paddingBottom',
+    'top', 'left', 'right', 'bottom',
+    'width', 'height'
+  ]
+  const warn = R.memoize(
+    prop => {
+      console.warn(`${prop} animates slowly. Use a transform instead`)
+    }
+  )
+  const check = R.pipe(
+    Object.values,
+    R.map(Object.keys),
+    R.reduce(R.concat, []),
+    R.uniq,
+    R.filter(v => R.contains(v, badProperties)),
+    R.forEach(warn)
+  )
+
+  const checkOnce = R.memoize(check)
+
+  const checkOffThread = keyframes => setTimeout(() => checkOnce(keyframes), 0)
+
+  return checkOffThread
+})()
+
 /**
  * Turn a keyframes object into a gsap timeline
  *
@@ -16,6 +45,7 @@ import normalizeKeyframes from './normalizeKeyframes'
 
 const makeTimeline = R.curry(
   (keyframes, element) => {
+    checkForBadProperties(keyframes)
     const normalizedKeyframeArray = normalizeKeyframes(keyframes)
     const [head, ...tail] = normalizedKeyframeArray
     debug('making timeline', {head, tail, keyframes, normalizedKeyframeArray})
